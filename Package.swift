10c975a684390fb4b4c369ef1ae7ec266067776b
sup34 <124162512+sup34@users.noreply.github.com>
2025-03-04 14:41:20 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "opencv2",
-                      url: "https://github.com/sup34/opencv-spm/blob/main/opencv2.xcframework.zip",
+                      url: "https://filebin.net/jmfd9oelh9ilts7d/opencv2.xcframework.zip",
                       checksum: checksum),
         .target(
             name: "opencv2-dependencies",
